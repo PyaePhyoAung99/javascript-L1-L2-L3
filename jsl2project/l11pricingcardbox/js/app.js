0152b0c0e96f1ad39893001e760b398b3f49cd7c
philipphilp <pyaephyoaung711999@gmail.com>
2024-01-10 16:54:30 +0630

    10 Jan 2024

---
+++ jsl2project/l11pricingcardbox/js/app.js
@@ -0,0 +1,19 @@
+const getcheckbox = document.getElementById('toggle-checkbox');
+const getchecklabel = document.getElementById('toggle-label');
+const getbasic = document.getElementById('basic');
+const getprof = document.getElementById('prof');
+const getmst = document.getElementById('mst');
+
+getchecklabel.addEventListener('click', function(){
+   // console.log('I am working');
+
+   if(getcheckbox.checked){
+      getbasic.textContent = 10;
+      getprof.textContent = 20;
+      getmst.textContent = 30;
+   }else{
+      getbasic.textContent = 120;
+      getprof.textContent = 240;
+      getmst.textContent = 360;
+   }
+});
