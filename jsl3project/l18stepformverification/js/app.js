625ecaaa572b08f893126082947b2cd5ff2afe0f
philipphilp <pyaephyoaung711999@gmail.com>
2024-11-28 14:03:30 +0630

    28 Nov 2024

--- jsl3project/l18stepformverification/js/app.js
+++ jsl3project/l18stepformverification/js/app.js
@@ -1,9 +1,13 @@
 var getdots = document.getElementsByClassName('dot');
-var getpages = document.getElementsByClassName('page');
-   // console.log(getpages);   // HTMLCollection
+var getpages = document.getElementsByClassName('page');    // console.log(getpages);   // HTMLCollection
 var getform = document.getElementById('form');
 var getprevbtn = document.getElementById('prevbtn');
 var getnextbtn = document.getElementById('nextbtn');
+const getrstcontainer = document.getElementById('result-container');
+
+var objkeys = ['email', 'password', 'firstname', 'lastname', 'dob', 'phone', 'address'];
+
+var datas = [];
 
 var curridx = 0;
 
@@ -29,7 +33,6 @@ function dotindicator(num){
 
    getdots[num].className += ' active';
 }
-
 
 function gonow(num){
       // console.log(num);
@@ -75,10 +78,29 @@ function gonow(num){
             // console.log(curridx);
 
          if(curridx >= getpages.length){
-            getform.submit();
+            // getform.submit();
+            
+            getform.style.display = 'none';
+            getrstcontainer.style.display = 'block';
+
+            result(datas);
+
+            return false;
          }
          showpage(curridx);
 }
+
+function* genfun(){
+   var index = 0;
+
+   while(index < objkeys.length){
+      yield index++;
+   }
+}
+// console.log(genfun().next().value);   // 0
+
+let gen = genfun()
+      // console.log(genfun().next().value);   // 1
 
 function formvalidation(){
    var valid = true;
@@ -93,12 +115,62 @@ function formvalidation(){
       if(getcurrinput[x].value === ''){
          getcurrinput[x].classList.add('invalid');
          valid = false;
+      }else{
+            // console.log('X value is = ', x);
+            // console.log(objkeys[x]);
+            // console.log(getcurrinput[x].value);
+            // console.log('Gen value is = ', gen.next().value);
+
+         // Method 1
+
+         // const keys = objkeys[gen.next().value];
+         //    // console.log(keys);
+         // const values = getcurrinput[x].value;
+
+         // const obj = {
+         //    [keys]:values
+         // datas.push(obj);
+         // }
+
+      // Method 2
+
+      // const keys = objkeys[gen.next().value];
+      // const values = getcurrinput[x].value;
+
+      // var obj = {};
+      //    obj[keys] = values;
+      //       datas.push(obj);
+
+      // Method 3
+
+      const keys = objkeys[gen.next().value];
+      const values = getcurrinput[x].value;
+         datas.push({[keys]:values});
       }
    }
 
    if(valid){
       getdots[curridx].className += ' done';
    }
-
    return valid;
+}
+
+function result(data){
+      // console.log(data);
+
+   getrstcontainer.innerHTML = `
+      <ul>
+         <li>Name : ${data[2].firstname} ${data[3].lastname}</li>
+         <li>Email : ${data[0].email}</li>
+         <li>Date Of Birth : ${data[4].dob}</li>
+         <li>Phone Number : ${data[5].phone}</li>
+         <li>Address : ${data[6].address}</li>
+      </ul>
+
+      <button type="submit" class="submit-btn" onclick="submitbtn()">Apply Now</button>
+   `;
+}
+
+function submitbtn(){
+   getform.submit();
 }
